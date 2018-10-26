e51c4a0243cd7915236e0a2516f23284d0048d6e
ps0305 <pankajsingh0305@gmail.com>
2018-10-26 13:10:17 +0530

    palindrome_checker

--- 02_Palindrome/index.js
+++ 02_Palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+	const reversed = str.split('').reverse().join('');
+	return str === reversed;
+}
 
 module.exports = palindrome;
