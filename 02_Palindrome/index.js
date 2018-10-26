90e65ff0569a444232e1d4c8e5d89030e1647db1
ps0305 <pankajsingh0305@gmail.com>
2018-10-26 14:20:43 +0530

    palindrome_every_helper

--- 02_Palindrome/index.js
+++ 02_Palindrome/index.js
@@ -7,9 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+
+// arr.every(callback(element[, index[, array]])[, thisArg])
 function palindrome(str) {
-	const reversed = str.split('').reverse().join('');
-	return str === reversed;
+	return str.split('').every((char, i) => {
+		return char ===str[str.length - i -1];
+	});
 }
 
 module.exports = palindrome;
+
+
+/*function palindrome(str) {
+	const reversed = str.split('').reverse().join('');
+	return str === reversed;
+}*/
