160424cfe8eff4101fe4b72b3e599b9960b97f97
ps0305 <singh.pankaj@live.com>
2019-10-30 20:12:08 +0530

    Update Sliding_window.js

--- Problem_solving_patterns/Sliding_window.js
+++ Problem_solving_patterns/Sliding_window.js
@@ -3,6 +3,29 @@
 write a function called maxSubarraySum which accepts an array of integers and a number called n.
 The function should calculate the maximum sum of n consecutive elements in the array.
 */
+// Naive approach ==> Big O - O(n*2)
+function maxSubarraySum(arr, num) {
+    if ( num > arr.length){
+      return null;
+    }
+    var max = -Infinity;
+    for (let i = 0; i < arr.length - num + 1; i ++){
+      temp = 0;
+      for (let j = 0; j < num; j++){
+        temp += arr[i + j];
+      }
+      if (temp > max) {
+        max = temp;
+      }
+    }
+    return max;
+  }
+  
+  maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+  
+  
+////////////////////////////////////////////////////////////////////////// 
+// refactored => O(n) 
 function maxSubarraySum(arr, num){
     let maxSum = 0;
     let tempSum = 0;
